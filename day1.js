686ac2f935e07c3a1ca767eed02350168801207a
tabrez <tabrez>
2023-12-17 03:10:17 +0000

    loopy loop

--- day1.js
+++ day1.js
@@ -1000,3 +1000,30 @@ qbfvpnxsix3four1lfone
 9one9pjtnncsqzhcszp5`;
 
 const input = data.split('\n')
+
+function isNumber(char) {
+    return !isNaN(char);
+}
+
+let total = 0;
+for (code of input) {
+    let joinedNumber = "";
+    for (let x = 0; x < code.length; x++) {
+        if (isNumber(code[x])) {
+            joinedNumber += code[x];
+            break;
+        }
+
+    }
+
+    for (let x = code.length-1; x >= 0; x--) {
+        if (isNumber(code[x])) {
+            joinedNumber += code[x];
+            break;
+        }
+    }
+
+    total += parseInt(joinedNumber, 10);
+}
+
+console.log(total);
