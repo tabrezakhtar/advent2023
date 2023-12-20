d1376ee525f71227388fc5090fa7e00b3d7ae054
tabrez <tabrez>
2023-12-20 10:05:52 +0000

    do part 2 of day 1

---
+++ main.js
@@ -0,0 +1,24 @@
+import input from "./data.js";
+
+const numberMap = {
+    "one": "o1e",
+    "two": "t2o",
+    "three": "t3e",
+    "four": "f4r",
+    "five": "f5e",
+    "six": "s6x",
+    "seven": "s7n",
+    "eight": "e8t",
+    "nine": "n9e",
+}
+
+let total = 0;
+for (let code of input) {
+    Object.keys(numberMap).map(k => code = code.replaceAll(k, numberMap[k]));
+
+    const numbers = code.split("").filter(c => !isNaN(c));
+    const numberFromCode = numbers[0] + numbers[numbers.length-1];
+    total += parseInt(numberFromCode, 10);
+}
+
+console.log(total);
